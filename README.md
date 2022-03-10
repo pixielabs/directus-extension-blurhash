3d543810be866f5c2f175caf061e5fa239de7871
Jalada <126989+jalada@users.noreply.github.com>
2022-03-10 15:17:49 +0000

    Use blurhash, tidyup Typescript errors

---
+++ README.md
@@ -0,0 +1,38 @@
+# Setup
+
+1. `npm i`
+2. Add a migration to your Directus project:
+
+    ```
+    // extensions/migrations/xxxxx-add-blurhash.js
+    module.exports = {
+      async up(knex) {
+        await knex.schema.alterTable('directus_files', (table) => {
+          table.text('blurhash');
+        });
+      },
+
+      async down(knex) {
+        await knex.schema.alterTable('directus_files', (table) => {
+          table.dropColumn('blurhash');
+        });
+      },
+    };
+    ```
+
+# Build
+
+```
+npm run build
+```
+
+# Install
+
+Copy the output of `dist/` after running the build command into your Directus
+project under `./extensions/hooks/blurhash/`.
+
+# Developing
+
+```
+npx directus-extension build -o /path/to/de-pury-cms/extensions/hooks/blurhash/index.js  -w
+```
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,62 @@
 import { defineHook } from '@directus/extensions-sdk';
+import { encode } from 'blurhash';
 
-export default defineHook(({ filter, action }, {services, database}) => {
+// External dependency; already included in Directus
+const sharp = require('sharp');
+
+const bufferToBlurhash = (buffer: Buffer) => (
+  new Promise((resolve, reject) => {
+    sharp(buffer)
+      .raw()
+      .ensureAlpha()
+      .resize(32, 32, { fit: "inside" })
+      .toBuffer((err: Error, buffer: Buffer, { width, height }: {width: number, height: number}) => {
+        if (err) return reject(err);
+        resolve(encode(new Uint8ClampedArray(buffer), width, height, 4, 4));
+      }); 
+  })
+);
+
+export default defineHook(({ action }, {services, database}) => {
   const AssetsService = services.AssetsService;
   const ItemsService = services.ItemsService;
   // ctx has database, schema, accountability.
   // See https://docs.directus.io/extensions/hooks/#action
   action('files.upload', async ({key}, ctx) => {
+    // Check that images have a blurhash field, if not, warn that migration
+    // needs to be run.
+    if (!ctx.schema) {
+      console.warn("[Blurhash] Was not given schema. Can't do anything");
+      return;
+    }
+
+    if (!ctx.schema.collections.directus_files?.fields?.blurhash) {
+      console.warn('[Blurhash] Migration needs to be run to add blurhash field to images');
+      return;
+    }
+
     const assetService = new AssetsService({knex: database, schema: ctx.schema});
     const itemService = new ItemsService('directus_files', {knex: database, schema: ctx.schema});
 
-    const result = await assetService.getAsset(key, {width: 10, quality: 80});
+    // Make the image smaller to reduce time taken to generate blurhash.
+    // Could experiment with dimensions for different effects.
+    console.log('[Blurhash] Fetching resized image');
+    const result = await assetService.getAsset(key, {width: 200, quality: 80});
 
-    // TODO: CHECK FOR MIME TYPE!
+    // List of file types from 
+    // <https://github.com/directus/directus/blob/main/api/src/services/assets.ts>
+    if (!['image/jpeg', 'image/png', 'image/webp', 'image/tiff'].includes(result.file.type)) {
+      console.log('[Blurhash] Not an image, skipping');
+      return;
+    }
 
-    const chunks = [];
-    result.stream.on('data', chunk => {
-      chunks.push(chunk.toString('base64'));
+    let buf = Buffer.from('');
+    result.stream.on('data', (chunk: Uint8Array) => {
+      buf = Buffer.concat([buf, chunk]);
     });
-    result.stream.on('end', () => {
-      itemService.updateOne(key, {base64: chunks.join('')});
+    result.stream.on('end', async () => {
+      const blurhash = await bufferToBlurhash(buf);
+      itemService.updateOne(key, {blurhash});
     });
   });
 });
